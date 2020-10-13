151c18b711be3390759cf7dcd5da185b3ab598ce
Wadhah mahrouk <wadhah.mahroug15@gmail.com>
2020-10-13 18:28:05 +0100

    ras placeholder

--- data/writing.static.ts
+++ data/writing.static.ts
@@ -2,12 +2,17 @@ export const writing = {
   cs: `
   The IEEE Computer Society INSAT SB Chapter is the premier source of information, inspiration, and collaboration in INSAT’s computer science community. Connecting them to multiple resources and professionals in the field.
   CS Chapter was founded by IEEEians with a passion for Software and Computer Science in March of 2015 and has since moved from strength to strength. Today we are home for more than 130 members, 20 training sessions and 6 events.`,
-  ras: '',
+
+  ras: ` The IEEE Computer Society INSAT SB Chapter is the premier source of information, inspiration, and collaboration in INSAT’s computer science community. Connecting them to multiple resources and professionals in the field.
+  CS Chapter was founded by IEEEians with a passion for Software and Computer Science in March of 2015 and has since moved from strength to strength. Today we are home for more than 130 members, 20 training sessions and 6 events.`,
+
   ias: `IEEE Industry Applications Society is one of the largest societies within IEEE, the IAS focuses specifically on the unique needs of industry and commerce and is a source of professional power to its nearly 14,000 worldwide members.
   Within INSAT IAS Chapter, the main goal is to reinforce the exchange of scientific knowledge in industrial concepts and operations.`,
+
   embs: `EEE Engineering in Medicine and Biology Society (EMBS) is the world’s largest international society of biomedical engineers.
   The organization’s 12,000 members reside in some 97 countries around the world.
   EMBS provides its members with access to the people, practices, information, ideas and opinions that are shaping one of the fastest growing fields in science`,
+
   wie:
     'IEEE Women in Engineering (WIE) is the largest international professional organization dedicated to promoting women engineers and scientists and inspiring girls around the world to follow their academic interests to a career in engineering',
   pes: `IEEE PES INSAT STUDENT CHAPTER was founded in 2016; it is the third chapter of PES in Tunisia Section and has about 150 members from IEEE INSAT SB with various academic backgrounds.
